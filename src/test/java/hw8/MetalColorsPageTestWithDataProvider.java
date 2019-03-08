222e34d9e4a0a47822dfc770b2c006179134fc8a
Ruslan <ruslan.sultan2016@mail.ru>
2019-03-08 23:31:06 +0300

    Homework 8 corrected

--- src/test/java/hw8/MetalColorsPageTestWithDataProvider.java
+++ src/test/java/hw8/MetalColorsPageTestWithDataProvider.java
@@ -7,12 +7,14 @@ import com.epam.jdi.light.ui.html.PageFactory;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import org.testng.annotations.*;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.lang.reflect.Type;
+import java.util.Map;
 
 import static base.jdi.entities.User.PETER;
 import static enums.HeaderMenu.METALS_COLORS;
@@ -22,18 +24,15 @@ public class MetalColorsPageTestWithDataProvider {
     private Object[][] jsonDataProvider() throws FileNotFoundException {
         JsonElement jsonElement = new JsonParser().parse(new FileReader
                 ("src/test/resources/hw8/JDI_ex8_metalsColorsDataSet.json"));
-        List<MetalColorsFormStateDP> statesList = new ArrayList<>();
-        int i = 1;
         // TODO Your script should not depends on test data names
         // TODO Take a look on TypeToken from Gson library
-        while (jsonElement.getAsJsonObject().get("data_" + i) != null) {
-            JsonElement dataSet = jsonElement.getAsJsonObject().get("data_" + (i++));
-            statesList.add(new Gson().fromJson(dataSet, MetalColorsFormStateDP.class));
-        }
-        Object[][] result = new Object[statesList.size()][1];
+        Type itemsMapType = new TypeToken<Map<String, MetalColorsFormStateDP>>() {
+        }.getType();
+        Map<String, MetalColorsFormStateDP> mapItemsDes = new Gson().fromJson(jsonElement, itemsMapType);
+        Object[][] result = new Object[mapItemsDes.size()][1];
         int j = 0;
-        for (Object[] each : result) {
-            each[0] = statesList.get(j++);
+        for (MetalColorsFormStateDP stateDP : mapItemsDes.values()) {
+            result[j++][0] = stateDP;
         }
         return result;
     }
